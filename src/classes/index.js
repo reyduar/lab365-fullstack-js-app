079038c1cace1c16971ac4e86ba915dd6d31e458
ariel.duarte <arielpy@gmail.com>
2023-02-25 17:58:17 -0300

    M1S04-Ex feat: Added code about execise of data person

--- src/classes/index.js
+++ src/classes/index.js
@@ -3,3 +3,5 @@ export { CaixaItem } from "./caixa-registradora/caixa-item.class";
 export { Produto } from "./caixa-registradora/produto.class";
 export { Conta } from "./conta-bancaria/conta.class";
 export { ContaPoupanca } from "./conta-bancaria/contaPoupanca.class";
+export { Person } from "./data-person/dataPerson.class";
+export { Profissao } from "./data-person/profissao.class";
--- src/pages/semana4/index.js
+++ src/pages/semana4/index.js
@@ -1,3 +1,4 @@
 import "./padaria-tech-mega-plus/padariaTechMegaPlus";
 import "./banco-poupancito/bancoPoupancito";
 import "./banco-poupancito-2/bancoPoupancito2";
+import "./data-person/dataPerson";
